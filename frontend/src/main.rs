d91e9fcce0a9d4d4179ef7ea00ecccbb4b16040e
S3llen3 <s3llen3@gmail.com>
2022-10-26 16:00:07 +0200

    Icon

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -8,7 +8,7 @@ fn app(cx: Scope) -> Element {
     cx.render(rsx! {
         div {
             Router {
-                h1 { style: "color: red;", "test" }
+                h1 { style: "color: red;", "LOOOOOOL" }
                 Route { to: "/", p { "Home" } Link { to: "/about", "About"} }
                 Route { to: "/about", p { "About" } Link { to: "/", "Home"} }
             }
