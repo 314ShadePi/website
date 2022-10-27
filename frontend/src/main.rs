baab3a2a5636cb6ba78f129e4781ec8d468fcc80
S3llen3 <s3llen3@gmail.com>
2022-10-27 13:09:45 +0200

    Snapshot 1

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,15 +4,20 @@ mod core;
 use dioxus::prelude::*;
 
 use crate::components::pages::core::page;
+use crate::core::s_page;
 
 fn main() {
     dioxus::web::launch(app);
 }
 
+static PAGES: state::Storage<Vec<s_page::Page>> = state::Storage::new();
+
 fn app(cx: Scope) -> Element {
+    PAGES.set(vec![s_page::Page { to: "/", name: "Home" }]);
     cx.render(rsx! {
             Router {
                 page::page {
+                    state: &PAGES,
                     to: "/",
                     name: "Home",
                     should_be_on_navbar: false,
@@ -24,6 +29,7 @@ fn app(cx: Scope) -> Element {
                     })
                 }
                 page::page {
+                    state: &PAGES,
                     to: "/about",
                     name: "Home",
                     should_be_on_navbar: true,
