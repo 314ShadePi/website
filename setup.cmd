4d942911a76b55a307468caa0f9801e57a506499
314ShadePi <erikkirkegaardstage@gmail.com>
2022-11-13 14:12:53 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- setup.cmd
+++ setup.cmd
@@ -1,12 +1,12 @@
-cd ./frontend
+cd .\frontend
 trunk build
-cd ../admin-dash
+cd ..\admin-dash
 trunk build
 
-cd ../backend
+cd ..\backend
 mkdir frontend
 mkdir admin-dash
 cd frontend
-mklink /D ../../frontend/dist ./dist
+mklink /D ..\..\frontend\dist .\dist
 cd ../admin-dash
-mklink /D ../../admin-dash/dist ./dist
+mklink /D ..\..\admin-dash\dist .\dist
