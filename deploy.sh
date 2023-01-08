fbf77a0eb0474dfcec59ca40b4b5e7edf0637583
314ShadePi <erikkirkegaardstage@gmail.com>
2023-01-08 17:06:07 +0100

    Backend not needed.  Fixes #16

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-docker compose --profile deploy down --rmi all
-docker compose --profile deploy up --force-recreate --build -d
--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-cd ./frontend || exit
-mkdir ./dist
-
-cd ../backend || exit
-mkdir frontend
-cd frontend || exit
-ln -s ../../frontend/dist ./dist
